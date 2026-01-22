b20ce7a008ffa19d4c5192d05fdcbe9264b311b8
Luchithaj <it23631724@my.sliit.lk>
2026-01-22 18:40:10 +0530

    Initial file setup

---
+++ to_do_flutter/lib/pages/home_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:to_do_flutter/util/todo_tile.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.green[200],
+      appBar: AppBar(
+        title: Text('To Do'),
+      ),
+      body: ListView(
+        children: [
+          ToDoTile(),
+        ]
+      )
+    );
+  }
+}
